f9c8e430ed3e622ae3da88a7a32a89233a2a155e
TeodoraAlina <alina.teower@gmail.com>
2023-03-04 14:52:06 +0000

    Create welcome_customer function to greet customer

--- run.py
+++ run.py
@@ -13,8 +13,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pasta-la-vista')
 
-menu = SHEET.worksheet("Menu")
 
-data = menu.get_all_values()
+def welcome_customer():
+    """
+    Greet customer
+    """
+    print("Welcome to Pasta la Vista,")
+    print("where all you can eat is delicious pasta!\n")
+    print("We will first need some information from you")
+    print("then you can proceed to order.\n")
+    print("Choose wisely, it is heard that our pasta")
+    print("...can become addictive.\n")
 
-print(data)
+
+welcome_customer()
